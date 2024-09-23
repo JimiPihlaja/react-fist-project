bed24e52184f49229e419a19b2eb710be214afb5
jimi <jimi.pihlajakangas@gmail.com>
2024-09-23 15:19:37 +0300

    tehtävä7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -8,7 +8,7 @@ function App() {
       amount: 94.12,
       date: new Date("2022-07-14"),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2023-01-12") },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date("2022-12-12") },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -19,7 +19,7 @@ function App() {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date("2022-05-12"),
+      date: new Date("2023-01-09"),
     },
   ];
 
