9e21112a772197db6aaf120b195a4d1141982e97
Jimi <Jimi.Pihlajakangas@gmail.com>
2024-09-24 16:37:02 +0300

    tehtävä11

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,22 +2,37 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
         const [enteredTitle, setEnteredTitle] = useState('');
-        const [Amount, setEnteredAmount] = useState('');
+        const [enteredAmount, setEnteredAmount] = useState('');
         const [enteredDate, setEnteredDate] = useState('');
 
         const titleChangeHandler = (event) => {
-            console.log(event.target.value);
+           setEnteredTitle(event.target.value);
         }
         const amountChangeHandler = (event) => {
-            console.log(event.target.value);
+            setEnteredAmount(event.target.value);
         }
         const dateChangeHandler = (event) => {
-            console.log(event.target.value);
+            setEnteredDate(event.target.value);
         }
+
+        const submitHandler = (event) => {
+            event.preventDefault();
+
+            const expenseData = {
+                title: enteredTitle,
+                amount: enteredAmount,
+                date: new Date(enteredDate)
+            };
+            //console.log(expenseData);
+            props.onSaveExpenseData(expenseData);
+            setEnteredTitle('');
+            setEnteredAmount('');
+            setEnteredDate('');
+        };
     return(
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
