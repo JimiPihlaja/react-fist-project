bacbb51abe39feab5f0a17952d1c7e187415ad27
jimi <jimi.pihlajakangas@gmail.com>
2024-10-07 14:14:00 +0300

    tehtävä 15

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,17 +14,25 @@ const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
 };
 
+const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+});
+
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {props.items.map((expense) => (
+            {filteredExpenses.length === 0 ? (
+                <p>No expenses found for {filteredYear}.</p>
+            ): (
+            filteredExpenses.map((expense) => (
           <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
           />
-            ))}
+            ))
+            )}
           </Card>
     );
 }
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './ExpensesFilter.css';
 
 const ExpensesFilter = (props) => {
-
     const dropdownChangeHandler = (event) => {
         props.onChangeFilter(event.target.value);
     };
@@ -12,7 +11,10 @@ const ExpensesFilter = (props) => {
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select onChange={dropdownChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+        <option value='2025'>2025</option>
+        <option value='2024'>2024</option>
+        <option value='2023'>2023</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
